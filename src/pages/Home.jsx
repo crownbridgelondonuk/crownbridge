e79ed798d25784c535934eac6ba337d7eb79a4d4
Ismail Silat <ismailsilat7@gmail.com>
2025-08-17 03:43:33 +0500

    Add Home Section - Completed

---
+++ src/pages/Home.jsx
@@ -0,0 +1,31 @@
+import "../home.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import Header from "./components/Header";
+import WhyFoundation from "./components/WhyFoundation";
+import WhereNext from "./components/WhereNext";
+import Banner from "./components/Banner";
+import USP from "./components/USP";
+import FAQ from "./components/FAQ";
+import WhoShouldJoin from "./components/WhoShouldJoin";
+import SkipALevels from "./components/SkipALevels";
+import Unsure from "./components/Unsure";
+
+export default function Home() {
+  return (
+    <div id="home">
+      <Header />
+      <WhyFoundation />
+      <WhereNext />
+      <Banner />
+      <USP />
+      <FAQ />
+      <WhoShouldJoin />
+      <div className="CTA">
+        <SkipALevels />
+        <hr />
+        <Unsure />
+      </div>
+    </div>
+  );
+}
