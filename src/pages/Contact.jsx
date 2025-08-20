385070f45a6737f40c3e0ad4acfde7a51eed2118
Ismail Silat <ismailsilat7@gmail.com>
2025-08-20 08:15:18 +0000

    Add content for contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,46 @@
+import "../contact.css"
+import emailIcon from "../assets/email.svg"
+import addressIcon from "../assets/address.svg"
+import phoneIcon from "../assets/phone.svg"
 
-
-export default function Home() {
+export default function Contact() {
     return (
-        <h1>Contact Us</h1>
+        <div id="contact">
+            <div className="embed-map-responsive">
+                <div className="embed-map-container">
+                    <iframe
+                        className="embed-map-frame"
+                        src="https://maps.google.com/maps?width=600&height=400&hl=en&q=Hexis%20College&t=&z=16&ie=UTF8&iwloc=B&output=embed"
+                        allowFullScreen
+                        loading="lazy"
+                        referrerPolicy="no-referrer-when-downgrade"
+                    ></iframe>
+                </div>
+            </div>
+
+            <div className="findus">
+                <h3>Find Us</h3>
+                <div className="findCards">
+                    <div className="address findCard">
+                        <img src={addressIcon} alt="Address Icon" />
+                        <h4>Address</h4>
+                        <p>D-29, Delhi Mercantile Society Delhi CHS P.E.C.H.S., Karachi, Pakistan</p>
+                        <p>7 Tully Drive, Paddock Wood, TN12 6FP, UK</p>
+                    </div>
+                    <div className="phone findCard">
+                        <img src={phoneIcon} alt="Phone Icon" />
+                        <h4>Phone</h4>
+                        <p>+92-336-6643947</p>
+                        <p>+44-77-1548052</p>
+                    </div>
+                    <div className="email findCard">
+                        <img src={emailIcon} alt="Email Icon" />
+                        <h4>Email</h4>
+                        <p>info@crownbridge.co.uk</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     );
 }
