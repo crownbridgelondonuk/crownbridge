a18182f42b61faf2e18316ed3af109bd80f363f9
Ismail Silat <ismailsilat7@gmail.com>
2025-08-21 21:29:27 +0500

    Embed GForm

--- src/pages/Apply.jsx
+++ src/pages/Apply.jsx
@@ -1,7 +1,17 @@
-
+import "../apply.css";
 
 export default function Apply() {
-    return (
-        <h1>Apply</h1>
-    );
+  return (
+    <div className="responsive-form">
+      <iframe
+        title="Crownbridge Application Form"
+        src="https://docs.google.com/forms/d/e/1FAIpQLSemdO8gkycZedpDRpP32ogA2owf7RXAiwF6vZ8uy17QHQoCAA/viewform?embedded=true"
+        loading="lazy"
+        className="responsive-iframe"
+        allowFullScreen
+      >
+        Loading…
+      </iframe>
+    </div>
+  );
 }
