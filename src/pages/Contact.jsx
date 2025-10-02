c7d227feb9d9f5517ae37755c13b11810d7dbe42
Ismail Silat <ismailsilat7@gmail.com>
2025-10-02 16:23:57 +0500

    Update map link

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,12 +9,14 @@ export default function Contact() {
             <div className="embed-map-responsive">
                 <div className="embed-map-container">
                     <iframe
-                        className="embed-map-frame"
-                        src="https://maps.google.com/maps?width=600&height=400&hl=en&q=Hexis%20College&t=&z=16&ie=UTF8&iwloc=B&output=embed"
-                        allowFullScreen
-                        loading="lazy"
-                        referrerPolicy="no-referrer-when-downgrade"
-                    ></iframe>
+                    className="embed-map-frame"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3619.5472353968053!2d67.06937599999999!3d24.879308800000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3eb33f60de397099%3A0x2ec33f5094b30dc8!2sCrownbridge%20-%20London%20International%20College!5e0!3m2!1sen!2s!4v1759403594022!5m2!1sen!2s"
+                    width="600"
+                    height="400"
+                    allowFullScreen=""
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                    ></iframe>                    
                 </div>
             </div>
 
